0799396ad53f4e5e2ad9b455a96f43e98d803dcf
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-08 09:11:16 +0000

    Add like_id and following_id

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,3 +1,4 @@
+
 from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Follower
@@ -6,22 +7,19 @@ from .models import Follower
 class FollowerSerializer(serializers.ModelSerializer):
     """
     Serializer for the Follower model
-    The create method handles the unique constraint on 'owner' and 'followed'
+    Create method handles the unique constraint on 'owner' and 'followed'
     """
-    owner = serializers.ReadOnlyField(source='owner.usrname')
-    followed_name = serializers.ReadOnlyField(source='followed.usrname')
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
 
-    class Meta():
+    class Meta:
         model = Follower
         fields = [
-            'id', 'owner', 'created_at',
-            'followed', 'followed_name',
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
         ]
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({
-                'detail': 'possible dublicate'
-            })
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -8,18 +8,16 @@ class LikeSerializer(serializers.ModelSerializer):
     Serializer for the Like model
     The create method handles the unique constraint on 'owner' and 'post'
     """
-    owner = serializers.ReadOnlyField(source='owner.usrname')
+    owner = serializers.ReadOnlyField(source='owner.username')
 
-    class Meta():
+    class Meta:
         model = Like
-        fields = [
-            'id', 'created_at', 'owner', 'post',
-        ]
+        fields = ['id', 'created_at', 'owner', 'post']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
-                'detail': 'possible dublicate'
+                'detail': 'possible duplicate'
             })
